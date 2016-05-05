d79a5e4f6a9d3de97adf059660ed65604e4cc5e0
ColeXm <846366712@qq.com>
2016-05-04 20:36:56 -0700

    1

---
+++ BaseVC.xctemplate/BaseVC/___FILEBASENAME___.h
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------
+ 文件名称 : ___FILEBASENAME___
+ 创建作者 : ___FULLUSERNAME___
+ 创建时间 : ___DATE___
+ 文件描述 :
+ ---------------------------------------------------------------------*/
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+@interface ___FILEBASENAME___: ___VARIABLE_cocoaTouchSubclass___
+
+@end
---
+++ BaseVC.xctemplate/BaseVC/___FILEBASENAME___.m
@@ -0,0 +1,52 @@
+//                    _oo0oo_
+//                   o8888888o
+//                   88" . "88
+//                   (| -_- |)
+//                   0\  =  /0
+//                 ___/`___'\___
+//               .' \\|     |// '.
+//              / \\|||  :  |||// \
+//             / _||||| -:- |||||_ \
+//            |   | \\\  _  /// |   |
+//            | \_|  ''\___/''  |_/ |
+//            \  .-\__  '_'  __/-.  /
+//          ___'. .'  /--.--\  '. .'___
+//        ."" '<  .___\_<|>_/___. '>' "".
+//     | | :  `_ \`.;` \ _ / `;.`/ - ` : | |
+//     \ \  `_.   \_ ___\ /___ _/   ._`  / /
+//  ====`-.____` .__ \_______/ __. -` ___.`====
+//                   `=-----='
+//
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                  佛祖保佑无bug
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAME___ ()
+
+@end
+
+@implementation ___FILEBASENAME___
+
+#pragma mark - init 初始化
+/* 初始化界面*/
+-(void)initUI{
+    
+}
+/* 初始化数据*/
+-(void)initData{
+    
+}
+
+#pragma mark - Action 控件事件
+#pragma mark - Notification
+#pragma mark - KVO
+#pragma mark - TableViewDelegate 代理
+#pragma mark - Custom CallBack  普通回调
+#pragma mark - Set/Get 重写设值/取值
+#pragma mark - Private 乱七八糟私有方法
+#pragma mark - NetWork CallBack 网络回调
+-(void)onResponse:(id)obj{
+}
+
+@end
